cc5b6aa2d4f1fe977bb13dc2115f6f132e1db774
binhussen <mhdhsna@gmail.com>
2022-09-06 14:42:04 +0300

    Delete the boilerplate from CRA

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +11,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-reportWebVitals();
