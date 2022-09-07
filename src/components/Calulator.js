a5f3b6863a665bd1fd3332a4823a981ef1ac5541
binhussen <mhdhsna@gmail.com>
2022-09-07 12:13:30 +0300

    Handle Result

--- src/components/Calulator.js
+++ src/components/Calulator.js
@@ -5,86 +5,94 @@ import './Calculator.css';
 import calculate from '../logic/calculate';
 
 class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			total: 0,
+			next: null,
+			operation: null,
+		};
+	}
+
+	handleResult = (e) => {
+		const name = e.target.textContent;
+		this.setState((prev) => ({
+			...prev,
+			total: name,
+		}));
+	};
 
 	handleKeyPress = (e) => {
-	  const keyValue = e.target.textContent;
-	  const result = calculate(this.state, keyValue);
-	  this.setState(result);
+		const keyValue = e.target.textContent;
+		const result = calculate(this.state, keyValue);
+		this.setState(result);
 	};
 
 	render() {
-	  return (
-  <div className="calculator">
-    <div className="box">
-      <div className="result four-cell">0</div>
-      <button type="button" className="key">
-        AC
-      </button>
-      <button type="button" className="key">
-        +/-
-      </button>
-      <button type="button" className="key">
-        %
-      </button>
-      <button type="button" className="key operator">
-        /
-      </button>
-      <button type="button" className="key">
-        7
-      </button>
-      <button type="button" className="key">
-        8
-      </button>
-      <button type="button" className="key">
-        9
-      </button>
-      <button type="button" className="key operator">
-        *
-      </button>
-      <button type="button" className="key">
-        4
-      </button>
-      <button type="button" className="key">
-        5
-      </button>
-      <button type="button" className="key">
-        6
-      </button>
-      <button type="button" className="key operator">
-        -
-      </button>
-      <button type="button" className="key">
-        1
-      </button>
-      <button type="button" className="key">
-        2
-      </button>
-      <button type="button" className="key">
-        3
-      </button>
-      <button type="button" className="key operator">
-        +
-      </button>
-      <button type="button" className="key two-cell">
-        0
-      </button>
-      <button type="button" className="key">
-        .
-      </button>
-      <button type="button" className="key operator">
-        =
-      </button>
-    </div>
-  </div>
-	  );
+		return (
+			<div className='calculator'>
+				<div className='box'>
+					<div className='result four-cell'>0</div>
+					<button type='button' className='key'>
+						AC
+					</button>
+					<button type='button' className='key'>
+						+/-
+					</button>
+					<button type='button' className='key'>
+						%
+					</button>
+					<button type='button' className='key operator'>
+						/
+					</button>
+					<button type='button' className='key'>
+						7
+					</button>
+					<button type='button' className='key'>
+						8
+					</button>
+					<button type='button' className='key'>
+						9
+					</button>
+					<button type='button' className='key operator'>
+						*
+					</button>
+					<button type='button' className='key'>
+						4
+					</button>
+					<button type='button' className='key'>
+						5
+					</button>
+					<button type='button' className='key'>
+						6
+					</button>
+					<button type='button' className='key operator'>
+						-
+					</button>
+					<button type='button' className='key'>
+						1
+					</button>
+					<button type='button' className='key'>
+						2
+					</button>
+					<button type='button' className='key'>
+						3
+					</button>
+					<button type='button' className='key operator'>
+						+
+					</button>
+					<button type='button' className='key two-cell'>
+						0
+					</button>
+					<button type='button' className='key'>
+						.
+					</button>
+					<button type='button' className='key operator'>
+						=
+					</button>
+				</div>
+			</div>
+		);
 	}
 }
 export default Calculator;
