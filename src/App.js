5864ffeb5c3d2c5c04c20be5e905fa33ac3be68d
binhussen <mhdhsna@gmail.com>
2022-09-13 15:40:25 +0300

    Config and prepare route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calulator';
+import Home from './components/Home';
+import Navbar from './components/NavBar';
+import Quote from './components/Quote';
 
-const App = () => <Calculator />;
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,6 +9,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
