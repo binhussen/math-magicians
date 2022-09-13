88efef138f66ec54e6e3806f24435a20fc140178
binhussen <mhdhsna@gmail.com>
2022-09-13 15:27:17 +0300

    Add NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+const Navbar = () => (
+  <header>
+    <h1>Math Magicians</h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Calculator</li>
+        <li>Quote</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navbar;
