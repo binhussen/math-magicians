e6e0ec904704acb5bd176d7265de6f4155ca2bf5
binhussen <mhdhsna@gmail.com>
2022-09-13 15:29:01 +0300

    Add Quote Component

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import './Quote.css';
+
+const Quote = () => (
+  <section>
+    <h2>Quote of the Day</h2>
+    <p>
+      Mathematics is not about numbers, equations, computations, or
+      algorithms: It is about understanding. -- William Paul Thurston
+    </p>
+  </section>
+);
+
+export default Quote;
