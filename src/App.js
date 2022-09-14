ee630dba83a4bdc190d75c30104d26349b7b905f
binhussen <mhdhsna@gmail.com>
2022-09-14 12:10:25 +0300

    Update Test for App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Quote from './components/Quote';
@@ -6,14 +6,16 @@ import Header from './components/Header';
 import Calculator from './components/Calculator';
 
 const App = () => (
-  <div className="main">
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quote" element={<Quote />} />
-    </Routes>
-  </div>
+  <BrowserRouter>
+    <div className="main">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,8 +8,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
