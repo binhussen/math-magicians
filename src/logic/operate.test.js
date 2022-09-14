e358799bb4e0a0fc3c22b424abe4ee921947f3bb
Farida-Hamid <faridaaddala@gmail.com>
2022-09-14 13:22:25 +0200

    Test errors in the operation component

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -4,4 +4,12 @@ describe('Testing the operate component:', () => {
   it('1. Testing addition:', () => {
     expect(operate(52, 12, '+')).toBe('64');
   });
+
+  it('2. Testing divide by zero error:', () => {
+    expect(operate(10, 0, '÷')).toBe("Can't divide by 0.");
+  });
+
+  it('3. Test not an operation error:', () => {
+    expect(() => {operate(10, 0, '*')}).toThrow(Error);
+  });
 });
